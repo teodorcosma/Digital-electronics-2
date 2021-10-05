80e4d42dada2d4bd20a3075e0ed2fea92b8883a5
student <student@PC-635.urel.feec.vutbr.cz>
2021-10-05 15:08:33 +0200

    laB3

---
+++ Labs/02-leds/02-leds/02-leds/main.c
@@ -0,0 +1,18 @@
+/*
+ * 02-leds.c
+ *
+ * Created: 05.10.2021 13:00:12
+ * Author : student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
